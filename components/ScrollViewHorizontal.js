4560d22ea8c1bcaa7e3ba66ea2a28f6b9ed28614
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-10-25 10:15:06 +0700

    feat: create scroll view horizontal component

---
+++ components/ScrollViewHorizontal.js
@@ -0,0 +1,110 @@
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+const rewardsData = [
+  {
+    id: 1,
+    name: "Es Teh Manis",
+    points: 40,
+    image: require("../assets/esteh.png"),
+  },
+  {
+    id: 2,
+    name: "Kopi Susu",
+    points: 50,
+    image: require("../assets/kopisusu.png"),
+  },
+  {
+    id: 3,
+    name: "Boba Milk Tea",
+    points: 60,
+    image: require("../assets/boba.png"),
+  },
+  {
+    id: 4,
+    name: "Es Teh Manis",
+    points: 40,
+    image: require("../assets/esteh.png"),
+  },
+  {
+    id: 5,
+    name: "Kopi Susu",
+    points: 50,
+    image: require("../assets/kopisusu.png"),
+  },
+  {
+    id: 6,
+    name: "Boba Milk Tea",
+    points: 60,
+    image: require("../assets/boba.png"),
+  },
+  {
+    id: 7,
+    name: "Es Teh Manis",
+    points: 40,
+    image: require("../assets/esteh.png"),
+  },
+  {
+    id: 8,
+    name: "Kopi Susu",
+    points: 50,
+    image: require("../assets/kopisusu.png"),
+  },
+  {
+    id: 9,
+    name: "Boba Milk Tea",
+    points: 60,
+    image: require("../assets/boba.png"),
+  },
+];
+
+export default function ScrollViewHorizontal() {
+  return (
+    <View style={{ backgroundColor: "#fff", paddingHorizontal: 18 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <View>
+          <Text style={{ fontWeight: "900", fontSize: 18 }}>
+            Redeem Your Rewards
+          </Text>
+          <Text style={{ fontSize: 12 }}>Earn more to get your rewards</Text>
+        </View>
+        <Text style={{ fontWeight: "bold", color: "orange" }}>SEE ALL</Text>
+      </View>
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        style={{ backgroundColor: "#fff" }}
+      >
+        {rewardsData.map((reward) => (
+          <TouchableOpacity key={reward.id} style={{ padding: 8 }}>
+            <View
+              style={{
+                backgroundColor: "white",
+                borderRadius: 8,
+                elevation: 2,
+                width: 150,
+                height: 150,
+              }}
+            >
+              <Image
+                style={{ width: 150, height: 150 }}
+                source={reward.image}
+              />
+            </View>
+            <Text style={{ fontWeight: "bold", marginTop: 12 }}>
+              {reward.name}
+            </Text>
+            <Text style={{ fontWeight: "bold", color: "orange" }}>
+              {reward.points} Pts
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
