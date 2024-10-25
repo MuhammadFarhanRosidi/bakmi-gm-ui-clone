334b51b41840e9aff0a244d900783ae27e25b322
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-10-25 10:14:34 +0700

    feat: create a gmpoint component

---
+++ components/GMPoint.js
@@ -0,0 +1,28 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function GMPoint() {
+  return (
+    <TouchableOpacity
+      style={{
+        elevation: 2,
+        padding: 12,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        height: "50",
+        backgroundColor: "white",
+        borderRadius: 4,
+      }}
+    >
+      <View>
+        <Text style={{ fontWeight: "bold" }}>Bakmi GM Point</Text>
+        <Text>
+          40 will expired on 22 Feb 2024.{" "}
+          <Text style={{ textDecorationLine: "underline", color: "blue" }}>
+            See Details
+          </Text>
+        </Text>
+      </View>
+      <Text style={{ fontWeight: "bold" }}>40</Text>
+    </TouchableOpacity>
+  );
+}
